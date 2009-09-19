6ed8fe2c0e9974d61a54da46b560e291a650d495
swdyh <youhei@gmail.com>
2009-09-19 20:06:44 +0900

    added build script using crxmake.

---
+++ build.rb
@@ -0,0 +1,11 @@
+require 'rubygems'
+require 'crxmake'
+
+CrxMake.make(
+  :ex_dir => "./src",
+  :pkey   => "./autopagerize_for_chrome.pem",
+  :crx_output => "./autopagerize_for_chrome.crx",
+  :verbose => true,
+  :ignorefile => /\.swp/,
+  :ignoredir => /\.(?:svn|git|cvs)/
+)
