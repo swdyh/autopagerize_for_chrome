4804581d80f031886f641a2009c60500046b5221
swdyh <youhei@gmail.com>
2010-01-27 17:29:42 +0900

    added options page

--- src/background.js
+++ src/background.js
@@ -12,6 +12,17 @@ function init() {
                 con.postMessage(res)
             })
         }
+        if (port.name == "settingsChannel") {
+console.log('receive settingChannel')
+            port.onMessage.addListener(function(message, con) {
+                var res = {}
+                var keys = ['exclude_patterns']
+                for (var i = 0; i < keys.length; i++) {
+                    res[keys[i]] = localStorage[keys[i]]
+                }
+                con.postMessage(res)
+            })
+        }
     })
 
     urls.forEach(function(url) {
