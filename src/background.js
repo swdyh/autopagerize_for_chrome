9cf938964fa17229be8a1fb30012e8ff9ee675f9
swdyh <youhei@gmail.com>
2010-02-09 01:19:28 +0900

    load local SITEINFO and caching

--- src/background.js
+++ src/background.js
@@ -1,12 +1,17 @@
-var siteinfo = []
-var urls = ['http://wedata.net/databases/AutoPagerize/items.json']
+var SITEINFO_IMPORT_URLS = [
+    'http://wedata.net/databases/AutoPagerize/items.json',
+]
+var CACHE_EXPIRE = 24 * 60 * 60 * 1000
+var siteinfo = {}
 window.onload = init
 
 function init() {
     chrome.extension.onConnect.addListener(function(port) {
         if (port.name == "siteinfoChannel") {
             port.onMessage.addListener(function(message, con) {
-                var res = siteinfo.filter(function(s) {
+                var res = SITEINFO_IMPORT_URLS.reduce(function(r, url) {
+                    return r.concat(siteinfo[url].info)
+                }, []).filter(function(s) {
                     return message.url.match(s.url)
                 })
                 con.postMessage(res)
@@ -31,13 +36,78 @@ function init() {
             })
         }
     })
+}
 
-    urls.forEach(function(url) {
-        get(url, function(res) {
-            var s = JSON.parse(res.responseText)
-            var d = s.map(function(i) { return i.data })
-            Array.prototype.push.apply(siteinfo, d)
+function loadLocalSiteinfoCallback(data) {
+    var url = 'http://wedata.net/databases/AutoPagerize/items.json'
+    var cache = JSON.parse(localStorage['cacheInfo'] || '{}')
+    if (!cache[url]) {
+        siteinfo[url] = {
+            url: url,
+            expire: new Date().getTime() - 1,
+            info: reduceWedataJSON(data)
+        }
+        cache[url] = siteinfo[url]
+        localStorage['cacheInfo'] = JSON.stringify(cache)
+    }
+    else {
+        siteinfo[url] = cache[url]
+    }
+    refreshSiteinfo()
+}
+
+function reduceWedataJSON(data) {
+    var r_keys = ['url', 'nextLink', 'insertBefore', 'pageElement']
+    var info = data.map(function(i) {
+        return i.data
+    }).filter(function(i) {
+        return ('url' in i)
+    })
+    if (info.length == 0) {
+        return []
+    }
+    else {
+        info.sort(function(a, b) {
+            return (b.url.length - a.url.length)
         })
+        return info.map(function(i) {
+            var item = {}
+            r_keys.forEach(function(key) {
+                if (i[key]) {
+                    item[key] = i[key]
+                }
+            })
+            return item
+        })
+    }
+}
+
+function refreshSiteinfo() {
+    var cache = JSON.parse(localStorage['cacheInfo'] || '{}')
+    SITEINFO_IMPORT_URLS.forEach(function(url) {
+        if (!cache[url] || (cache[url].expire && new Date(cache[url].expire) < new Date())) {
+            var callback = function(res) {
+                if (res.status != 200) {
+                    return
+                }
+                var info = reduceWedataJSON(JSON.parse(res.responseText))
+                if (info.length == 0) {
+                    return
+                }
+                siteinfo[url] = {
+                    url: url,
+                    expire: new Date().getTime() + CACHE_EXPIRE,
+                    info: info
+                }
+                cache[url] = siteinfo[url]
+                localStorage['cacheInfo'] = JSON.stringify(cache)
+            }
+            try {
+                get(url, callback)
+            }
+            catch(e) {
+            }
+        }
     })
 }
 
@@ -52,3 +122,4 @@ function get(url, callback, opt) {
     xhr.send(null)
     return xhr
 }
+
